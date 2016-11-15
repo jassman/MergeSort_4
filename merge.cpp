65cbb9b68dfab27f55b7c153b9d65ffbc5549f08
jassman <jalonso.are@gmail.com>
2016-11-15 01:28:20 +0100

    Coste en fichero

--- merge.cpp
+++ merge.cpp
@@ -10,30 +10,44 @@ using namespace std;
 
 void MezclaOrd (vector<VElement>& , vector<VElement>&, int , int );
 void Mezclar(vector<VElement> &, vector<VElement> &, int , int , int);
+void evaluar (unsigned ini, unsigned fin, int inc, int (*t)(unsigned),ostream &file);
+int funcionAux (unsigned);
     
 int main ()
 {
 	srand(time(NULL));
+
+	ofstream fichero, merge_r;
+
 	int n = 10;
-	vector <VElement> v_orig; 
-	vector <VElement> v_aux;
 	int i;
 
-	for (i = 0; i < n; i++)
-	{
-		v_orig.push_back(1 + rand()%200);
-    		cout << v_orig.at(i).get() << " ";
-	}
-
-	MezclaOrd (v_orig, v_aux, 0, n-1);
-	cout << endl;
-	for (i = 0; i < n; i++)
-	{
-	    	cout << v_orig.at(i).get() << " ";
-	}
-	cout << endl;
-
+	fichero.open ("ficheros.dat");
+	evaluar (50, 500,  50, funcionAux, fichero);
+	fichero.close();
 	return 0;
+}
+
+int funcionAux (unsigned n)
+{
+	vector <VElement> vec_orig (n); 
+	vector <VElement> vec_aux (n);
+	int a,c;
+	unsigned i;
+
+	for (i = 0; i < n; i++)
+	{
+		vec_orig.push_back(1 + rand()%200);
+		vec_orig[i].initNComparison();
+		vec_orig[i].initNAssign();
+	}
+
+	MezclaOrd (vec_orig, vec_aux, 0, n-1);
+
+	a = VElement::getNAssign ();
+	c = VElement::getNComparison ();
+
+	return (a+c);
 }
 
 void MezclaOrd (vector<VElement>& v_orig, vector<VElement>& v_aux, int i, int j)
@@ -59,8 +73,6 @@ void Mezclar(vector<VElement> &v_orig, vector<VElement> &v_aux, int izquierda, i
 	i=izquierda;
 	j=m+1;
 	k=izquierda;
-
-	v_aux=v_orig;
 
 	while((i <= m) && (j <= derecha))
 	{
@@ -100,4 +112,13 @@ void Mezclar(vector<VElement> &v_orig, vector<VElement> &v_aux, int izquierda, i
 	{
 		v_orig.at(aux)=v_aux.at(aux);
 	}
+}
+
+void evaluar (unsigned ini, unsigned fin, int inc, int (*f) (unsigned),ostream &file)
+{
+	unsigned i;
+	for (i = ini;i <= fin; i = i + inc)
+	{
+		file << i << " " << f(i) << endl;
+	} 
 }
